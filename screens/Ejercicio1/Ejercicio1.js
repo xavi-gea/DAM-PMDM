9e0c6c1bf029d161365c1f85ea61256f416fc439
Xavi <144837551+xavi-gea@users.noreply.github.com>
2025-02-13 16:34:01 +0100

    Ejercicio1

---
+++ screens/Ejercicio1/Ejercicio1.js
@@ -0,0 +1,109 @@
+import { View, Button, StyleSheet, Text, TextInput, Pressable, Image } from 'react-native';
+import { CameraView, useCameraPermissions } from 'expo-camera';
+import { useEffect, useRef, useState } from 'react';
+
+import { MaterialIcons } from '@expo/vector-icons';
+
+export default function Ejercicio1() {
+
+  const [permision, setPermision] = useCameraPermissions();
+  const [cameraType, setCameraType] = useState('back');
+
+  const [photo, setPhoto] = useState(null);
+  
+  const camera = useRef(null);
+
+  const [startShooting, setStartShooting] = useState(false);
+
+  if (!permision) {
+    return <></>;
+  }    
+
+  if (!permision.granted) {
+    
+    return(
+      <View style={styles.container}>
+        <Button onPress={setPermision} title='Pedir permiso para la cámara'></Button>
+      </View>
+    );
+  }
+
+  async function takePicture() {
+      
+    const options = { quality: 0.5, base64: true };
+    const img = await camera.current.takePictureAsync(options);
+    setPhoto(img.uri);
+
+    setStartShooting(false);
+  }
+
+  if (startShooting) {
+    
+    return(
+      <View style={styles.container}>
+        <CameraView style={styles.camera} ref={camera} facing={cameraType}>
+          <View style={styles.buttonContainer}>
+            <Pressable 
+              style={styles.button} 
+              onPress={() => {setCameraType(cameraType === 'back' ? 'front' : 'back')}}
+            >
+              <Text style={styles.text}>Cambiar cámara</Text>
+
+              <MaterialIcons 
+                onPress={takePicture} 
+                name='play-circle-outline' 
+                size={75} 
+                color="black"
+              />
+            </Pressable>
+          </View>
+        </CameraView>
+      </View>
+    );
+
+  }else{
+
+    return (
+      <View style={styles.container}>
+        <Button onPress={() => setStartShooting(true)} title="Start Shooting" />
+        {photo != null ? (
+          <Image width="100%" height="50%" source={{ uri: photo }}/>
+        ) : (
+          <></>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50,
+  },
+  camera: { flex: 1 },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+    margin: 20,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 18,
+    color: "white",
+  },
+  buttonText: {
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    textAlignVertical: "center",
+    width: 80,
+    height: 80,
+    backgroundColor: "blue",
+  }
+});
